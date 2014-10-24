f6c3ea9f4be82cba0f3bfdf6f2030b40864f940d
Andre Midea <midea.andre@gmail.com>
2014-10-23 23:03:27 -0200

    Adicionando novas padginas

--- new/assets/js/routes.js
+++ new/assets/js/routes.js
@@ -14,6 +14,18 @@ angular.module('siteApp', ['ngRoute'])
     templateUrl: 'home.html',
     controller: 'SiteCtrl',    
   })
+  .when('/how', {
+    templateUrl: 'how.html',
+    controller: 'SiteCtrl',    
+  })
+  .when('/about', {
+    templateUrl: 'about.html',
+    controller: 'SiteCtrl',    
+  })
+  .when('/portfolio', {
+    templateUrl: 'portfolio.html',
+    controller: 'SiteCtrl',    
+  })
   .when('/contact', {
     templateUrl: 'contact.html',
     controller: 'SiteCtrl'
