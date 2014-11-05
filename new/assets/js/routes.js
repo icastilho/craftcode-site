f249de7bec4db664fe0e002525e2e381971c0ab3
Andre Midea <midea.andre@gmail.com>
2014-11-04 22:21:56 -0200

    contact

--- new/assets/js/routes.js
+++ new/assets/js/routes.js
@@ -1,7 +1,25 @@
 angular.module('siteApp', ['ngRoute'])
 
- .controller('SiteCtrl', function($scope, $routeParams) {
+ .controller('SiteCtrl', function($scope, $routeParams, $http) {
     $scope.sele = 'startups';
+    $scope.msg = {
+        name: '',
+        email: '',
+        subject: '',
+        text: ''
+    };
+    $scope.msgEnviada = false;
+    
+    $scope.sendMail = function (){
+        $http.get('https://api.mailgun.net/v2/samples.mailgun.org/messages', {
+            from: $scope.msg.email,
+            to: 'contato@craftcode.com.br',
+            subject: $scope.msg.subject,
+            text: $scope.msg.text
+        });
+        $scope.msg = {};
+        $scope.msgEnviada = true;
+    }
  })
 
 .config(function($routeProvider, $locationProvider) {
